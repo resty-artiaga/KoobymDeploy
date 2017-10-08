32b765f09d17e98cbcc6a29080225e806d69b1e6
Joane14 <joane.artiaga@gmail.com>
2017-10-08 17:51:39 +0800

    altered method

--- KoobymDeploy/src/main/java/com/koobym/dao/impl/RentalHeaderDaoImpl.java
+++ KoobymDeploy/src/main/java/com/koobym/dao/impl/RentalHeaderDaoImpl.java
@@ -61,7 +61,7 @@ public class RentalHeaderDaoImpl extends BaseDaoImpl<RentalHeader, Long> impleme
 		
 		Criteria criteria = getSessionFactory().getCurrentSession().createCriteria(RentalHeader.class);
 		criteria = criteria.createAlias("user", "user");
-		criteria = criteria.add(Restrictions.or(Restrictions.eq("status", "Approved"),Restrictions.eq("status", "Received")));
+		criteria = criteria.add(Restrictions.eq("status", "Received"));
 		criteria = criteria.add(Restrictions.eq("user.userId", new Long(userId)));
 		criteria = criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
 		flag = (List<RentalHeader>) criteria.list();
