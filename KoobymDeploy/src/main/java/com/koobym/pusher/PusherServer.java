71e1eb96c16ded27b82baa46de188254d10d964d
resty-artiaga <resty@ring.md>
2017-11-07 00:41:54 +0800

    asdf

--- KoobymDeploy/src/main/java/com/koobym/pusher/PusherServer.java
+++ KoobymDeploy/src/main/java/com/koobym/pusher/PusherServer.java
@@ -11,11 +11,16 @@ public class PusherServer {
 	private Pusher pusher;
 
 	public PusherServer() {
-		pusher = new Pusher("426875", "0aa2ef5ad16d9caba80a", "1dee74d0541f9fb0ca16");
-		pusher.setCluster("ap1");
+		/*
+		 * pusher = new Pusher("426875", "0aa2ef5ad16d9caba80a",
+		 * "1dee74d0541f9fb0ca16"); pusher.setCluster("ap1");
+		 */
 	}
 
 	public void sendNotification(UserNotification userNotification) {
-		pusher.trigger(Long.toString(userNotification.getUser().getUserId()), "notification-event", userNotification);
+		/*
+		 * pusher.trigger(Long.toString(userNotification.getUser().getUserId()),
+		 * "notification-event", userNotification);
+		 */
 	}
 }
